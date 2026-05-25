0af472ce258b31280ad152d085db7c6c767aa1e6
Miguel Monzon <miguel.monzon.lucio@gmail.com>
2026-05-25 17:28:23 -0500

    Actualizando el Dockerfile v4

--- run.py
+++ run.py
@@ -1,7 +1,10 @@
 import os
+print("=== Iniciando run.py ===")
 from app import create_app
+print("=== create_app importado ===")
 
 app = create_app()
+print("=== App creada ===")
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 10000))
